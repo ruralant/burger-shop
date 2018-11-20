6a1e27441631e72c7a639b006a3b213d8a755830
Antonio Rossi <info@antoniorossi.net>
2018-11-20 22:27:24 +0000

    sidebar implemented

--- src/components/Layout/Layout.module.js
+++ src/components/Layout/Layout.module.js
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Toolbar from '../Navigation/Toolbar/Toolbar.module';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer.module'
 
 import classes from './Layout.module.css';
 
-const layout = (props) => (
-  <>
-    <Toolbar />
-    <main className={classes.Content}>
-      {props.children}
-    </main>
-  </>
-);
+class Layout extends Component {
+  state = {
+    showSideDrawer: false
+  }
 
-export default layout
+  sideDrawerCloseHandler = () => {
+    this.setState({showSideDrawer: false})
+  }
+
+  sideDrawerToggleHandler = () => {
+    this.setState((prevState) => {
+      return {showSideDrawer: !prevState.showSideDrawer}
+    })
+  }
+
+  render() {
+    return (
+      <>
+        <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+        <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerCloseHandler} />
+        <main className={classes.Content}>
+          {this.props.children}
+        </main>
+      </>
+    )
+  }
+}
+
+export default Layout;
--- src/components/Navigation/Toolbar/Toolbar.module.js
+++ src/components/Navigation/Toolbar/Toolbar.module.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import Logo from '../../Logo/Logo.module';
-import NavigationItems from "../NavigationItems/NavigationItems.module";
+import NavigationItems from '../NavigationItems/NavigationItems.module';
+import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle.module';
 import classes from './Toolbar.module.css';
 
 const toolbar = (props) => (
   <header className={classes.Toolbar}>
-    <div>MENU</div>
-    <Logo />
-    <NavigationItems />
+    <DrawerToggle clicked={props.drawerToggleClicked}/>
+    <div className={classes.Logo}>
+      <Logo />
+    </div>
+    <nav className={classes.DesktopOnly}>
+      <NavigationItems />
+    </nav>
   </header>
 );
 
